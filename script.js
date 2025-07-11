f9e6b7ab6456d485339be70393f1c94d8cf78595
HAMZAH2310 <hamzahzulkhanbrk@gmail.com>
2025-07-11 08:52:57 +0700

    Header

---
+++ script.js
@@ -0,0 +1,16 @@
+//Header Menu Toogle
+
+const bar = document.getElementById("bar");
+const nav = document.getElementById("nav");
+
+bar.onclick = (e) => {
+  const icon = e.target.getAttribute("class")
+
+  if(icon === "fa-solid fa-bars"){
+    e.target.setAttribute("class","fa-solid fa-xmark")
+  }else{
+    e.target.setAttribute("class","fa-solid fa-bars")
+  }
+
+  nav.classList.toggle("showNav")
+}
